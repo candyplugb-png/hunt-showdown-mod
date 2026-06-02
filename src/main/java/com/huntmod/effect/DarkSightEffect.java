a6ff054240faf0639db1e0da16cf0f8cde9d3a2d
candyplugb-png <candyplugb@gmail.com>
2026-06-02 21:55:03 +0300

    Create DarkSightEffect.java

---
+++ src/main/java/com/huntmod/effect/DarkSightEffect.java
@@ -0,0 +1,19 @@
+package com.huntmod.effect;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+
+public class DarkSightEffect extends MobEffect {
+    public DarkSightEffect(MobEffectCategory category, int color) {
+        super(category, color);
+    }
+
+    @Override
+    public void applyEffectTick(LivingEntity entity, int amplifier) {}
+
+    @Override
+    public boolean isDurationEffectTick(int duration, int amplifier) {
+        return false;
+    }
+}
